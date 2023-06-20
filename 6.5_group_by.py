6b9ceb1b108cef4a53bd4c9be0c8d825c5b21d08
yaniv051293 <yaniv051293@gmail.com>
2023-06-20 14:46:15 +0300

    change group_by

--- 6.5_group_by.py
+++ 6.5_group_by.py
@@ -1,12 +1,10 @@
 def group_by(func, args):
-    dic = {}
-    for item in args:
-        dic.setdefault(func(item), []).append(item)
-    return dic
+    return {key: [item for item in args if func(item) == key] for key in set(map(func, args))}
 
 
 def main():
-    assert(group_by(len, ["hi", "bye", "yo", "try"])=={2: ["hi", "yo"], 3: ["bye", "try"]})
+    assert (group_by(len, ["hi", "bye", "yo", "try"]) == {2: ["hi", "yo"], 3: ["bye", "try"]})
+
 
 if __name__ == '__main__':
     main()
